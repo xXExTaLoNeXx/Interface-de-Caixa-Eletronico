3e464526b179daf12415499611aeaf9a98698a9a
xXExTaLoNeXx <125830468+xXExTaLoNeXx@users.noreply.github.com>
2023-06-22 20:40:25 -0300

    Update Bank.java
    Codigo finalizado, usando curso de programação java da Alura.

--- BankAcc/src/Bank.java
+++ BankAcc/src/Bank.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        int choice = 0;
 
-        int accValue = 2500;
+        double accValue = 1599.99;
 
-        String cifrValue = "R$ " + accValue;
+        String menu = """
+                ** Digite sua opção **
+                
+                1 - Consultar saldo
+                2 - Saque
+                3 - Deposito
+                4 - Sair;
+                
+                """;
         String name;
         String acc;
 
         System.out.println("###########################\n\nBem vindo ao banco Bank!!!\n\n###########################\n\n");
-      
+
         do {
             System.out.println("Por favor digite seu nome:");
             name = read.nextLine();
@@ -22,24 +31,34 @@ public class Bank {
             acc = read.nextLine();
         } while (acc.isEmpty());
 
-        System.out.println("\n***************************************\n\nDados iniciais do cliente\n\nNome: "+name+"\n\nTipo de conta: "+acc+"\n\nSaldo da conta: "+cifrValue+"\n\n***************************************\n\n");
-        
-        boolean leave = false;
-        while (!leave) {
-            System.out.println("Qual opção deseja acessar?\n1\n Consulta de saldo\n2 Deposito\n3 Saque\n4 Sair");
-            
-            int selection1 = read.nextInt();
-            read.nextLine();
+        System.out.println("\n***************************************\nDados iniciais do cliente\nNome: "+name+"\nTipo de conta: "+acc+"\nSaldo da conta: R$"+accValue+"\n***************************************\n");
 
-            if (selection1 == 1) {
-                System.out.println("Saldo disponivel: "+cifrValue);
-                System.out.println("Deseja realizar outra transação? (S/N)");
-                String selection2 = read.nextLine();
-                if (selection2.equals("N")) {
-                    leave = true;
-                    break;
+        while (choice != 4) {
+            System.out.println(menu);
+
+            choice = read.nextInt();
+            if (choice == 1) {
+                System.out.println("Saldo disponivel: R$"+accValue);
+            } else if (choice == 2) {
+                System.out.println("Bem vindo ao modulo de saque!!\nQuando gostaria de sacar?\nSaldo atual: R$"+accValue);
+                double withdraw = read.nextDouble();
+                if (withdraw <= accValue) {
+                    accValue -= withdraw;
+                    System.out.println("Saque realizado com sucesso\nSaldo atualizado R$"+accValue);
+                } else {
+                    System.out.println("Saldo insuficiente para realizar o saque");
                 }
+            } else if (choice == 3) {
+                System.out.println("Bem vindo ao modulo de depositos!!\nQuanto gostaria de depositar?\nSaldo atual: R$"+accValue);
+                double deposit = read.nextDouble();
+                accValue += deposit;
+                System.out.println("Deposito realizado com sucesso!!\nSaldo atualizado R$"+accValue);
+            } else if (choice == 4) {
+                System.out.println("Obrigado por utilizar nossos serviços!!");
+            } else{
+                System.out.println("Opção Invalida");
             }
         }
     }
 }
+
